43df3b20a69f64afcd778014a401b3e42c498fcc
ambrosekuo <ambrose.kuo@hotmail.com>
2025-12-24 16:26:21 -0500

    part 2 b with efficiency by counting only the beam

--- day7/main2.py
+++ day7/main2.py
@@ -35,15 +35,13 @@ def draw_tree_replace(tree):
 # question is do we update the array? probably on first attempt...
 
 # we find s and draw the line at the bottom
-def draw_starting_frame(tree):
+def get_first_beam(tree):
     updated_tree = copy.deepcopy(tree)
     # can just safely assume it's in the first row, since we can see both
     # our inputs
     s_row = {x: i for i,x in enumerate(updated_tree[0])}
     pos_of_s = s_row.get('S')
-    updated_tree[1][pos_of_s] = '|'
-
-    return updated_tree
+    return (1, pos_of_s)
 
 # we are done when we found any beams in the last row
 def is_timeline_done(tree):
@@ -54,57 +52,37 @@ def is_timeline_done(tree):
 # where there are no more splits. Thats why example should be so low at 40
 # timelines?
 def get_splt_tree(tree):
-    updated_tree = draw_starting_frame(tree)
-    level = 2 # note level starts at 0
-    split_count = 0
-    timelines = [updated_tree]
+    [level, beam_index] = get_first_beam(tree)
+    level = level+1
+    # type will be a tuple containing beams and splits per beam
+    beams = [(beam_index, 1)]
 
-    # the key here is that we can travel through all timelines at the same level
-    # at a time
-    while level < len(updated_tree):
-        # will update current timelines, but also iterate through tracked ones
-        timelines += update_all_next_split_frames(timelines, level)
-        print(len(timelines))
-        # print(len(timelines))
+    while level < len(tree):
+        row = tree[level]
+        for i, [beam_index, splits] in enumerate(beams):
+            if row[beam_index] == '^':
+                beams[i] = [beam_index-1, splits]
+                beams.append((beam_index+1, splits))
+
+        # collect the beams that are the same
+        beams_map = {}
+        for [beam_index, splits] in beams:
+            beams_map[beam_index] = beams_map.get(beam_index, 0) + splits
+        beams = [(beams_index, beams_map[beams_index]) for beams_index in beams_map]
+
         level +=1
 
-    # for timeline in timelines:
-    #     draw_tree(timeline)
+    return sum([splits for [_, splits] in beams])
 
-    return len(timelines)
-
-def update_all_next_split_frames(timelines, level):
-    new_timelines = []
-    for tree in timelines:
-        new_timelines += update_next_split_frame(tree, level)
-
-    return new_timelines
-
-# side effect
-def update_next_split_frame(tree, level):
-    previous_row = tree[level-1]
-    row = tree[level]
-    splitters = [(i, x) for i,x in enumerate(row) if x == '^']
-    beams = [(i, x) for i,x in enumerate(previous_row) if x == '|']
-
-    new_timelines = []
-
-    for [beam_i, beam] in beams:
-        # will assume that adjacents don't have splitters and empty space
-        # no ask in problem to handle this
-        # also will assume out of bounds doesn't exist
-        if row[beam_i] == '^':
-            new_timeline_tree = copy.deepcopy(tree)
-            # lets keep this as the current timeline of this tree
-            row[beam_i-1] = '|'
-            # new_timeline
-            new_timeline_row = new_timeline_tree[level]
-            new_timeline_row[beam_i+1] = '|'
-
-            new_timelines.append(new_timeline_tree)
-        else:
-            row[beam_i] = '|'
-
-    return new_timelines
 
 print(get_splt_tree(tree))
+
+
+"""
+ What makes what we had built via A quite ineffiicent is that we keep track of every
+ split as its own timeline. But we really only care about how many paths reaches
+ a point.
+ E.g. if 5 paths hit a splitter, we just now have 10 paths. But we don't actually
+ need to know how they get there. So we just need to keep track of splits
+ per trachyon beam.
+"""
